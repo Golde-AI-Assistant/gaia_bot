c1663292bc964427d746abb96604b6da43173872
Golde34 <nguyendongducviet2001@gmail.com>
2024-01-06 13:07:18 +0700

    create middleware command

---
+++ modules/ports/commands/middleware_command.py
@@ -0,0 +1,28 @@
+import requests
+import subprocess
+import json
+import os
+
+from gaia_bot.configs.port_configs import PORTS, DOMAIN, PORT_COMPONENTS
+from gaia_bot.modules.ports.commands.microservices_state.call_microservices import CallMicroservices
+
+
+class MiddlewareLoaderConnector:
+    def __init__(self):
+        self.middleware_bash = PORTS['middleware_loader']['shell_path']
+        self.microservice_state = CallMicroservices(self.middleware_bash, self.gaia_url)
+        
+        # intances
+        self.gaia_port = PORTS['gaia_connector']['port']
+        self.router = PORTS['middleware_loader']['router']
+        self.gaia_url = f"http://{DOMAIN}:{self.gaia_port}/{self.router}"
+            
+    def activate_middleware_command(self):
+        self.microservice_state.activate_service()
+    
+    def check_microservices_state(self):
+        response = requests.get(f"{self.gaia_url}/microservices-status")
+        
+        return response.json()
+        #Logic of microsercvices state will response which one is running, which one is not
+    
